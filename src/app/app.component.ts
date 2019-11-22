2a634021a1491048488e4727abe543c22538c74f
Irshadali Palsaniya <irshadali.palsaniya.sa@gmail.com>
2019-11-22 15:04:58 +0530

    Recipes Fprm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Button } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Team-Exception';
+
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  recipes = [
+    {
+      name: 'Burger',
+      chef: 'Ranveer Brar',
+      image: 'http://salemdigest.com/wp-content/uploads/2016/08/TITS_food1.jpg',
+      type: 'nv'
+    },
+    {
+      name: 'Italian Pasta',
+      chef: 'JM',
+      image: 'https://mariettasquaremarket.com/msm/wp-content/uploads/2018/12/Pita-Mediterranean-5.jpg',
+      type: 'v'
+    },
+    {
+      name: 'Chicken Maggie',
+      chef: 'Nisha Madhulika',
+      image: 'http://eatbook.sg/wp-content/uploads/2018/06/Century-Square-Food-Two-Hana.jpg',
+      type: 'nv'
+    },
+    {
+      name: 'Veg. Pulav',
+      chef: 'Tarla Dalal',
+      image: 'https://media-cdn.tripadvisor.com/media/photo-p/0e/75/7b/5d/photo3jpg.jpg',
+      type: 'v'
+    },
+  ];
+
 }
